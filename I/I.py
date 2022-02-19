c16aca859bc153036993f189094677372ddca62d
iamnitya <93179435+iamnitya@users.noreply.github.com>
2022-02-19 19:05:33 +0530

    Create I.py

---
+++ I/I.py
@@ -0,0 +1,9 @@
+#I
+n=7
+for i in range(1,n+1): 
+    for j in range(1,n+1): 
+        if((j==n//2+1 or i==1 or i==n)):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
