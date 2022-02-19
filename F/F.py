2aba0cb7f5aab8a37444ece22dafde603d20c12d
iamnitya <93179435+iamnitya@users.noreply.github.com>
2022-02-19 19:01:14 +0530

    Create F.py

---
+++ F/F.py
@@ -0,0 +1,9 @@
+#F
+n=7
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if(j==1) or (i==1) or (i==n//2+1):
+            print("*",end=' ')
+        else:
+            print(' ',end=' ')
+    print('')
