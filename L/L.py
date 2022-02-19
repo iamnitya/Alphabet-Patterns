50f1b7de8c53a19458438ea9adef50ffd3af73ed
iamnitya <93179435+iamnitya@users.noreply.github.com>
2022-02-19 19:08:12 +0530

    Create L.py

---
+++ L/L.py
@@ -0,0 +1,9 @@
+#L
+n=7
+for i in range(1,n+1): 
+    for j in range(1,n+1): 
+        if((j==1 or i==n)):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
