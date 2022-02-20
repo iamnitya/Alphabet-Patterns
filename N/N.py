c908f875f566f796d648e42c6edafbc0e8f3a3b2
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-02-20 19:48:59 +0530

    Create N.py

---
+++ N/N.py
@@ -0,0 +1,9 @@
+#N
+n=9
+for i in range(1,n+1): 
+    for j in range(1,n+1): 
+        if((j==1 or j==n) or (j==n//2+1 and i==n//2+1) or (i==j)):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
