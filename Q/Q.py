604f1cd49d45a59708e8b6b121d7a686a33a0711
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-02-23 09:34:08 +0530

    Create Q.py

---
+++ Q/Q.py
@@ -0,0 +1,9 @@
+#Q
+n=7
+for i in range(1,n+1): 
+    for j in range(1,n+1): 
+        if(j==1 and 1<i<=n-1) or (j==n and 1<i<=n-1) or (i==n and 1<j<=n-1) or (i==1 and 1<j<=n-1) or  (i==j and i>=n//2+1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
